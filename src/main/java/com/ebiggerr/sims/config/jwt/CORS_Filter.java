afc9c67f0de1fe61e6a2ae7b4afafc0bd498daf8
EbiggerR <kelvinlin900123@gmail.com>
2021-03-08 14:59:51 +0800

    SIMSBACK-4 #time 1h Setting up the configurations for JWT module.
    Signed-off-by: EbiggerR <kelvinlin900123@gmail.com>

---
+++ src/main/java/com/ebiggerr/sims/config/jwt/CORS_Filter.java
@@ -0,0 +1,43 @@
+/*
+ *  Copyright (C) 2020  Alexious Yong (Ebiggerr)
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.ebiggerr.sims.config.jwt;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CORS_Filter implements Filter{
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
+        httpServletResponse.setHeader("Access-Control-Allow-Credentials", "true");
+        httpServletResponse.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
+        httpServletResponse.setHeader("Access-Control-Max-Age", "3600");
+        httpServletResponse.setHeader("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Authorization, Origin, Accept, Access-Control-Request-Method, Access-Control-Request-Headers");
+        chain.doFilter(request, response);
+    }
+
+    public void init(FilterConfig filterConfig) {
+
+    }
+
+    public void destroy() {
+
+    }
+}
---
+++ src/test/java/com/ebiggerr/sims/service/timeDateTest.java
@@ -0,0 +1,55 @@
+/*
+ *  Copyright (C) 2020  Alexious Yong (Ebiggerr)
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.ebiggerr.sims.service;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class timeDateTest {
+
+    @Test
+    void convertFromTimeStampUsingTimeFROMSystemCurrentTimeMillis() {
+
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+
+        System.out.println(timeDate.convertFromTimeStamp(timestamp));
+
+        //System.out.println(date);
+
+    }
+
+    @Test
+    void convertFromTimeStampUsingEpoch(){
+
+        Long unixTime = System.currentTimeMillis();
+        long ut1 = Instant.now().getEpochSecond();
+
+        System.out.println(unixTime);
+        System.out.println(ut1);
+
+        System.out.println(timeDate.convertFromUnixTimeStamp(ut1));
+
+
+
+    }
+}
